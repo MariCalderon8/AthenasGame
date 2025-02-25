0ae2c102e88bf99072bee5030b169841b116cf92
Maria Camila Castro Cardona <castro.maria.5191@eam.edu.co>
2025-02-25 14:10:09 -0500

    ADD Cinematic and details to Chapter 3

---
+++ Assets/Scripts/ControladorSemilla.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class ControladorSemilla : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] public bool seedCollected = false;
+    public static ControladorSemilla Instance;
+    public GameObject objetoMostrar; 
+
+    private void Awake(){
+        if(ControladorSemilla.Instance == null){
+            ControladorSemilla.Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+
+        } else{
+            Destroy(gameObject);
+        }
+    }
+
+
+    void Start()
+    {
+        seedCollected = false;
+        objetoMostrar.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(seedCollected)
+        {
+
+        }
+    }
+
+    public void collectSeed(){
+        seedCollected = true;
+        objetoMostrar.SetActive(true);
+    }
+
+}
--- Assets/Scripts/Doors.cs
+++ Assets/Scripts/Doors.cs
@@ -1,6 +1,45 @@
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Doors
+public class Doors : MonoBehaviour
 {
-    
+    public GameObject objetoMostrar;
+    [SerializeField] private string siguienteEscena;
+    private bool jugadorInTrigger;
+
+    void Start()
+    {
+        objetoMostrar.SetActive(false);
+        jugadorInTrigger = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            objetoMostrar.SetActive(true);
+	    jugadorInTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            objetoMostrar.SetActive(false);
+	        jugadorInTrigger = false;
+        }
+    }
+
+    void Update()
+    {
+        if (jugadorInTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(siguienteEscena);
+        }
+    }
+
+
 }
